056855dd490cf66857149f05878c7f86c231a5d4
[01;31m[K"name"[m[K: "Alexandra Cooper <allie.g.cooper@gmail.com>
2018-04-12 10:43:09 -0400

    added functionality to see the dictionarys index of terms and ability to add new terms and definitions to the dictionary

--- dictionary.rb
+++ dictionary.rb
@@ -9,7 +9,7 @@ class Definition
 end 
 
 class Dictionary
-  attr_reader :library
+
   def initialize(definitions_array=[])
     @library = {}
     if !definitions_array.empty?
@@ -23,10 +23,20 @@ class Dictionary
     definition = @library[ennumerable]
     puts definition
   end 
+
+  def see_index
+    @library.keys.each { |term| puts term }
+  end 
+
+  def add_new(method_name, definition)
+    definition = Definition.new(method_name, definition)
+    @library[definition.method_name] = definition.explanation
+    puts "#{method_name} has been successfully added"
+  end 
 end 
 
 each = Definition.new("each", ".each is a Ruby method that can be called on an array. It takes a block of code and calls it once for each element, passing that element to the block.")
 
 dictionary = Dictionary.new([each])
 
-puts dictionary.get_info("each")
+dictionary.see_index
